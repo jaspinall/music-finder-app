79a654c005ef074f8e46a8af846c664d10255d4f
Julia Aspinall <juliaspinall@gmail.com>
2017-02-05 14:58:07 -0800

    refactor artist factory to persist state between different views

--- client/controllers/homeController.js
+++ client/controllers/homeController.js
@@ -4,32 +4,36 @@ angular
 
 function HomeController($scope, ArtistFactory) {
   $scope.searchVal = '';
-  $scope.featuredArtist = {};
-  $scope.relatedArtists = [];
+  $scope.featuredArtist = '';
+  $scope.relatedArtists = '';
   $scope.showArtist = false;
   $scope.showRelated = false;
   $scope.search = () => {
     ArtistFactory.fetchArtist($scope.searchVal).then((res) => {
-      if (res && res.data.artists.items) {
+      if (res && res.data.artists.items.length) {
         $scope.showArtist = true;
         const artist = res.data.artists.items[0];
-        $scope.featuredArtist = {
+        ArtistFactory.setArtist({
             'artistName': artist.name,
             'artistImg': artist.images[0].url,
             'artistId': artist.id
-        }
+        })
+        $scope.featuredArtist = ArtistFactory.getArtist();
       }
     })
     .then(() => ArtistFactory.fetchRelated($scope.featuredArtist.artistId))
     .then((res) => {
       $scope.showRelated = true;
-      $scope.relatedArtists = res.data.artists.map((artist) => {
+      ArtistFactory.setRelatedArtists(res.data.artists.map((artist) => {
         return ({
           'artistName': artist.name,
           'artistImg': artist.images[0].url,
           'artistId': artist.id
         })
-      })
+      }));
+      $scope.relatedArtists = ArtistFactory.getRelatedArtists();
     })
+    .then(() => ArtistFactory.fetchWikipedia($scope.featuredArtist.artistName))
+    .then((res) => console.log(res))
   }
 }
--- client/factories/artistFactory.js
+++ client/factories/artistFactory.js
@@ -3,7 +3,26 @@ angular
   .factory('ArtistFactory', ['$http', ArtistFactory]);
 
 function ArtistFactory($http) {
+  const featuredArtist = {};
+  const relatedArtists = [];
   return ({
+    getArtist: () => {
+      return featuredArtist;
+    },
+    setArtist: (artist) => {
+      featuredArtist.artistName = artist.artistName;
+      featuredArtist.artistImg = artist.artistImg;
+      featuredArtist.artistId = artist.artistId;
+    },
+    getRelatedArtists: () => {
+      return relatedArtists;
+    },
+    setRelatedArtists: (array) => {
+      console.log('setting artists')
+      array.forEach((artist) => {
+        relatedArtists.push(artist)
+      });
+    },
     fetchArtist: (artist) => {
       return (
         $http({
@@ -20,5 +39,13 @@ function ArtistFactory($http) {
         })
       )
     },
+    fetchWikipedia: (searchTerm) => {
+      return (
+        $http({
+          method: 'GET',
+          url: `http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=${searchTerm}`,
+        })
+      )
+    }
   })
 }
